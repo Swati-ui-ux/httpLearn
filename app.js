301af5f197b6a37c527ad16b5977d42c3f6574b5
Swati singh <swatigola274@email.com>
2026-03-19 15:17:59 +0530

    learn child proccess fork

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const http = require("http")
 const fs = require("fs")
-const {anotherFunction,requestFunction} = require("./route")
+const {fork } = require("child_process")
 
+const {anotherFunction,requestFunction} = require("./route")
+const child = fork("./file1.js")
+
+child.send({number:40})
+console.log("Computaion")
+child.on("message", (data) => {
+console.log("Result",data)
+})
 
 const server = http.createServer(requestFunction)
 
+
+// console.log(fib(40))
+console.log("done")
 server.listen(9000, () => {
     console.log("server running")
 })
---
+++ file1.js
@@ -0,0 +1,11 @@
+
+
+process.on("message", (data) => {
+
+    const fib = (n) => (n < 2 ? n : fib(n - 1) + fib(n - 2))
+    
+    let result = fib(data.number)
+    
+    process.send(result)
+})
+
