8a8f20122c174962ebf551ed63855765995dd613
Swati singh <swatigola274@email.com>
2026-03-20 12:46:49 +0530

    Learning

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const http = require("http")
 const fs = require("fs")
-const {fork } = require("child_process")
+// const {fork } = require("child_process")
+const {spawn } = require("child_process")
 
 const {anotherFunction,requestFunction} = require("./route")
-const child = fork("./file1.js")
-
-child.send({number:40})
+// const child = fork("./file1.js")
+// child.send({number:40})
+// child.on("message", (data) => {
+// console.log("Result",data)
+// })
+const server = http.createServer(requestFunction)
 console.log("Computaion")
-child.on("message", (data) => {
-console.log("Result",data)
+const child = spawn("cmd",["/c","echo %cd%"])
+
+child.stdout.on("data", (data) => {
+
+console.log(data.toString())
+})
+child.stderr.on("data", (err) => {
+
+console.log("error",err.toString())
 })
 
-const server = http.createServer(requestFunction)
+
+child.on("close", (code) => {
+console.log("Child proccess closed",code)
+})
 
 
 // console.log(fib(40))
-console.log("done")
+// console.log("done")
 server.listen(9000, () => {
     console.log("server running")
 })
